80fd826a4437b2e8143de32527869914bc958ace
kohina0318 <kohinat0318@gmail.com>
2023-08-13 21:05:19 +0530

    MerGe chapter 23

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -68,13 +68,20 @@ class __CartListState extends State<_CartList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return _cart.items.isEmpty ? 
+    "Noting to Show".text.xl2.makeCentered()
+    : ListView.builder(
       itemCount: _cart.items?.length,
       itemBuilder: (context,index)=>ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(
           icon: Icon(Icons.remove_circle_outline),
-          onPressed: (){},
+          onPressed: (){
+            _cart.remove(_cart.items[index]);
+            setState(() {
+              
+            });
+          },
           ),
           title: _cart.items[index].name.text.make(),
       )
--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cataloge/widgets/home_widgets/add_to_cart.dart';
 import 'package:flutter_cataloge/widgets/themes.dart';
 import "package:velocity_x/velocity_x.dart";
 
@@ -23,15 +24,7 @@ class HomeDetailPage extends StatelessWidget {
           buttonPadding: EdgeInsets.zero,
           children: [
             "\$${catalog.price}".text.bold.xl3.red500.make(),
-            ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(context.theme.floatingActionButtonTheme.backgroundColor),
-                      shape: MaterialStateProperty.all(StadiumBorder()),
-                    ),
-                    child: "Add to cart".text.xl.bold.make())
-                .wh(150, 50)
+           AddToCart(catalog: catalog).wh(120,50),
           ],
         ).p16(),
       ),
